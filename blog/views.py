8d8c3ba647bc63ea73471ba4f5841f7a28300f1b
hiroro <hrsi.ad@gmail.com>
2022-12-31 16:00:06 +0900

    Postモデル作成

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def frontpage(request):
+    return render(request, "blog/frontpage.html")
+
+def testpage(request):
+    return render(request, "testpage.html")
